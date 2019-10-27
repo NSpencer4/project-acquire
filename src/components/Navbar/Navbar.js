063a19fcede689d7cceafc7e2be0cd54180455e2
Chase <n.chase.spencer@gmail.com>
2019-10-27 19:07:49 -0400

    Styling fixes to the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,26 +6,35 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Grid from '@material-ui/core/Grid';
 
 class Navbar extends Component {
 
 	render() {
 		return (
-				<header className="App-header">
-					<div>
-						<AppBar position="static">
-							<Toolbar>
-								<IconButton edge="start" color="inherit" aria-label="menu">
-									<MenuIcon />
-								</IconButton>
-								<Typography variant="h6">
-									Project Acquire
-								</Typography>
-								<Button id="login-btn" color="inherit">Login</Button>
-							</Toolbar>
-						</AppBar>
-					</div>
-				</header>
+			<header className="App-header">
+				<div>
+					<AppBar position="static">
+						<Toolbar>
+							<Grid container direction="row" justify="center" alignItems="center">
+								<Grid item sm={1}>
+									<IconButton edge="start" color="inherit" aria-label="menu">
+										<MenuIcon/>
+									</IconButton>
+								</Grid>
+								<Grid item sm={6}>
+									<Typography variant="h6">
+										Project Acquire
+									</Typography>
+									</Grid>
+								<Grid item sm={4}>
+									<Button id="login-btn" color="inherit">Login</Button>
+								</Grid>
+							</Grid>
+						</Toolbar>
+					</AppBar>
+				</div>
+			</header>
 		);
 	}
 }
